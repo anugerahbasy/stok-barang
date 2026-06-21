db1b5889361d5d80fc5fd8af1caff3097eaf65b9
AkaxKing <121616431+anugerahbasy@users.noreply.github.com>
2026-06-22 06:17:16 +0700

    commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,7 +91,7 @@
                             </span>
                         </div>
 
-                        <form action="{{ route('logout') }}" method="POST">
+                        <form action="{{ route('logout') }}" method="POST" class="inline">
                             @csrf
                             <button type="submit" class="w-full text-left px-4 py-2.5 text-sm text-red-600 hover:bg-red-50 transition flex items-center gap-2 mt-1">
                                 <i class="fa-solid fa-right-from-bracket"></i> Keluar Sistem
