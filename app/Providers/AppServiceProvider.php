8b6c75558c2323336ac4bca17f9c10f3cd4a8b1c
AkaxKing <121616431+anugerahbasy@users.noreply.github.com>
2026-06-21 20:57:40 +0700

    commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,23 +4,29 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\URL; // Jangan lupa import URL
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        // Trik khusus agar <x-app-layout> langsung mengenali file kustom layouts.app kita
+        // 1. Force HTTPS jika di production
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
+        // 2. Registrasi Blade component kustom
         Blade::component('layouts.app', 'app-layout');
+    }
+
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        // Tempat untuk register service jika ada
     }
 }
