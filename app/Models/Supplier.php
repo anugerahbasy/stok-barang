ed22ebfa72aaa7acf5ea273e9f68027d4ea18559
AkaxKing <121616431+anugerahbasy@users.noreply.github.com>
2026-06-22 21:49:00 +0700

    commit

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -7,9 +7,16 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Supplier extends Model
 {
-    protected $fillable = ['supplier_code', 'company_name', 'pic_name', 'phone', 'address'];
+    // Tambahkan 'user_id' di sini agar bisa disimpan ke database
+    protected $fillable = [
+        'supplier_code', 
+        'company_name', 
+        'pic_name', 
+        'phone', 
+        'address', 
+        'user_id' 
+    ];
 
-    // Relasi: Satu supplier bisa memasok banyak jenis produk
     public function products(): HasMany
     {
         return $this->hasMany(Product::class);
