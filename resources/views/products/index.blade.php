f7f6f567f2b1cfc13330b7f025a4083737d30814
AkaxKing <121616431+anugerahbasy@users.noreply.github.com>
2026-06-22 17:51:55 +0700

    commit

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -117,11 +117,14 @@
                                 @endif
                             </td>
                         </tr>
-                    @empty
-                        <tr>
-                            <td colspan="6" class="px-6 py-10 text-center text-gray-400">Belum ada data produk barang.</td>
-                        </tr>
-                    @endforelse
+                    ...
+    @empty
+        <tr>
+            <td colspan="6" class="px-6 py-10 text-center text-gray-400">Belum ada data produk barang.</td>
+        </tr>
+    @endforelse  {{-- Pastikan ini adalah @endforelse, bukan @endforeach --}}
+    </tbody>
+</div>
                 </tbody>
         </div>
 
